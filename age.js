4cc43f44580464743d2738d0d0a7ad8436e27104
PAVAN <94223919+pavan0514@users.noreply.github.com>
2022-01-16 22:40:28 +0530

    Add files via upload

---
+++ age.js
@@ -0,0 +1,10 @@
+var name = prompt('Enter Your Name: ' )
+
+var age= prompt('Enter Your Age:')
+
+ageInDays = age*365
+
+remainingDays = (60-age)*365
+
+alert('Hello '+ name +'...\n Your Current Age:'+ageInDays+'\n You Will be there on earth only'+remainingDays+'\n Days.Hence  Enjoy Every Day in your Life Very less no of days Remaining ')
+
